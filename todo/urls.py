7fc98300878317683acfca380377553e168680e6
MatinSadeghii <byteboss2007@gmail.com>
2025-01-02 15:14:22 +0330

    delete path created

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .views import add,show,delete_task
+
 
 
 urlpatterns = [
     path('',views.add),
     path('show/', views.show, name='show'),
+    path('delete/<int:todo_id>/',delete_task,name='delete_task')
 ]
