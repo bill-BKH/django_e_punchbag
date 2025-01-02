cd98d965db5930b87f5826b64ed9d7f55893e665
MatinSadeghii <byteboss2007@gmail.com>
2025-01-02 23:22:09 +0330

    add page views updated

--- todo/views.py
+++ todo/views.py
@@ -6,12 +6,11 @@ from django.shortcut import get_object_or_404
 
 def add(request):
     if request.method == 'POST':
-        Text = request.POST.get('text')
-
-
-        X = Todo(text = Text)
+        text = request.POST.get('todo_text')
+        if text:
+            Todo.objects.create(text=text)
+    return render(request,'todo/add.html')
         
-
 def show(request):
     tasks = Todo.objects.all()
     return render(request,'todo/show.html',{'tasks':tasks})
